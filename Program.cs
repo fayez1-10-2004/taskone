943094a885bc701310d3a209fa05d2649f5f673a
fayez mohammed fayez <fayzm5575@gmail.com>
2025-10-02 23:06:55 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,178 @@
+﻿
+/*int x = Convert.ToInt32(     Console.ReadLine());
+int y = Convert.ToInt32(Console.ReadLine());
+int sum = x + y;
+int product = x * y;
+int difference = x - y;
+int quotient = x / y;
+Console.WriteLine($"Sum: {sum}, Product: {product}, Difference: {difference}, Quotient: {quotient}");
+///qustion 2
+
+
+if (x == y)
+{
+
+Console.WriteLine("  numbers are eual);
+}
+else
+{
+    Console.WriteLine("numbers are not equal");
+}
+
+
+/////qustion 3
+int age = Convert.ToInt32(Console.ReadLine());
+int salary = Convert.ToInt32(Console.ReadLine());
+    if(age>18&&salary > 5000){
+
+Console.WriteLine("accssept");
+}
+
+
+else{
+    Console.WriteLine("rejected");
+}
+*/
+
+// quistion 4
+//int x = 5;
+//    int y =10;
+//x=y;
+//x = +y;
+
+//qustio5
+
+//int x = 5;
+//x++;
+//Console.WriteLine(x);
+//x--;
+//Console.WriteLine(x);
+
+//qustion 6
+//int x = Convert.ToInt32(Console.ReadLine());
+//if (x % 2 == 0)
+//{
+
+//    Console.WriteLine($"{ x} is  even");
+//}
+//else
+//{
+//    Console.WriteLine($"{x} is odd");
+//}
+
+//qustion 8
+//int x = 5;
+//string name = "fayez";  
+//Console.WriteLine("my name is " + name + " and my age is " + x);
+//Console.WriteLine($"my name is {name} and my age is {x}");
+
+//qustion 9
+//DateTime today =DateTime.Now;
+//Console.WriteLine(today.ToString("dd/MM/yyyy"));
+
+//qustion 10
+
+//string body = "helo world";
+//string antherbody = body.Replace("world","fayez");
+//Console.WriteLine(tobody);
+//Console.WriteLine(ReferenceEquals(body,tobody));
+
+//qustion 11
+
+
+
+using System.Text;
+
+
+//StringBuilder sb =new StringBuilder("hello");
+//sb.Append("world");
+//sb.Append("fayez");
+//sb.Append("!");
+//sb.Remove(15,1);
+//Console.WriteLine(sb.ToString());
+
+
+//qustion 12
+//int grade = Convert.ToInt32(Console.ReadLine());
+//if (grade>=50)
+//{
+//       Console.WriteLine("pass");
+//}
+//else
+//{
+//    Console.WriteLine("fail");
+//}
+//Question 13
+//int NUM = 3;
+//switch (NUM)
+//{
+//case 1:
+//    Console.WriteLine("one");
+//    break;
+//    case 2: 
+//            Console.WriteLine("two");
+//        break;
+//        case 3:
+//        Console.WriteLine("three");
+//        break;
+//    default:
+//        Console.WriteLine("other");
+//        break;
+//}
+
+//Question 14
+
+
+//int NUM = 3;
+
+//    String result = NUM switch{
+//    1=>"one",
+//2=>"TWO",
+//3=>"THREE",
+//_=>"other"
+
+
+//};
+
+//question 15
+
+//for(int i =1; i<=10;i++)
+//{
+
+//    Console.WriteLine(i);
+//}
+
+//question 16
+
+//string[] names = { "fayez", "ahmed", "ali", "mostfa", "moahmmed" };
+
+
+//foreach (string i in names)
+//{
+//    Console.WriteLine(i);
+//}
+
+
+// question 17
+//int number ;
+// int sum =0;
+
+
+//while (true) 
+//{
+
+//    Console.WriteLine("enter you num");
+//    number = Convert.ToInt32(Console.ReadLine());
+
+//    if (number == 0) 
+//        break;
+
+
+//    sum += number;
+
+
+
+//}SSS
+//Console.WriteLine("total sum = " + sum);
+
+
